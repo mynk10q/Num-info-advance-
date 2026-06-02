e76e3dffdcb2a3565db1bf75c6619670103488d2
mynk10q <amit1100941@gmail.com>
2026-06-02 08:42:12 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
   try {
 
     // Backend API
-    const api = `https://usersxinfo-admin.onrender.com/api?key=UsersXinfofreeuersaa&type=${encodeURIComponent(type)}&term=${encodeURIComponent(term)}`;
+    const api = `https://usersxinfo-admin.onrender.com/api?key=lljeliye&type=${encodeURIComponent(type)}&term=${encodeURIComponent(term)}`;
 
     const response = await fetch(api);
     const data = await response.json();
